e9fc0f9a76d499b175bf910306f948dd92566ee4
LR-Hyzik07 <cussisaac007@gmail.com>
2024-06-01 13:19:20 +0100

    Add files via upload

---
+++ asset/Assignment2/pro.js
@@ -0,0 +1,20 @@
+// //Get the elements with class="column"
+// var elements =
+// document.getElementsByClassName("column");
+
+// //Declare a loop variable
+// var i;
+
+// //list view
+// function listview(){
+//     for (i = 0; i < elements.length; i++){
+//         elements[i].style.width = "100%";
+//     }
+// }
+
+// //Grid view
+// function gridView()  {
+//     for (i = 0; i < elements.length; i++){
+//         elements[i].style.width = "50%";
+//     }
+// }
